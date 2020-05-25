2eb2e0b44272e26814538f1419af7cf799597f32
cyproto <kyashkarande@gmail.com>
2020-05-25 01:10:25 +0100

    app: Initial work.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ViewResultComponent } from './view-result/view-result.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'view-result',
+    component: ViewResultComponent 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,64 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { LayoutModule } from '@angular/cdk/layout';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import * as firebase from 'firebase';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ViewResultComponent } from './view-result/view-result.component';
+import { MatRadioModule, MatInputModule, MatCheckboxModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule, MatProgressBarModule, MatIconModule, MatListModule, MatDividerModule, MatRippleModule, MatDialogModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBEOi6n55MMh52mQEvGZuindi2BD18jsn0",
+  authDomain: "e-assessment-using-blockchain.firebaseapp.com",
+  databaseURL: "https://e-assessment-using-blockchain.firebaseio.com",
+  projectId: "e-assessment-using-blockchain",
+  storageBucket: "e-assessment-using-blockchain.appspot.com",
+  messagingSenderId: "198687170091",
+  appId: "1:198687170091:web:c627e5c8b505301515146e",
+  measurementId: "G-GMVZP94GG0"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewResultComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    OverlayModule,
+    HttpClientModule,
+    AngularFireModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireStorageModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatProgressBarModule,
+    MatIconModule,
+    MatListModule,
+    MatDividerModule,
+    MatRippleModule,
+    MatRadioModule,
+    MatDialogModule,
+    FlexLayoutModule.withConfig({addFlexToParent: false}),
   ],
-  providers: [],
+  providers: [AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
